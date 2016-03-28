1bf2713ccdd4cead8ea042160dfac23484f039c5
Mario Wang <mario.wang@autodesk.com>
2016-03-28 15:12:27 +0800

    Created a new project food_finder'

--- ruby_essential_training/accessing_files.rb
+++ ruby_essential_training/accessing_files.rb
@@ -1,6 +1,16 @@
 file = File.new('file1.txt', 'w')
+file.puts "Ruby"
+file.print "programming\n"
+file.write "is "
+file << "fun"
 file.close
 
 File.open('file1.txt', 'r') do |file|
-  
+  while line = file.gets
+    puts "** " + line.chomp.reverse + " **"
+  end
+end
+
+File.open('file1.txt', 'r') do |file|
+  file.each_line { |line| puts line.upcase }
 end
