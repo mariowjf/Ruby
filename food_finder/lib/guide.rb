b3754fe2fc12bd386c6d2abbe3a3374ccab15d97
Mario Wang <mario.wang@autodesk.com>
2016-03-29 22:39:14 +0800

    Added a Restaurant class

---
+++ food_finder/lib/guide.rb
@@ -0,0 +1,32 @@
+require 'restaurant'
+
+class Guide
+  
+  def initialize(path=nil)
+    Restaurant.filepath = path
+    if Restaurant.file_exists?
+      puts "Found restaurant file."
+    elsif Restaurant.create_file
+      puts "Created restaurant file."
+    else
+      puts "Exiting.\n\n"
+      exit!
+    end      
+  end
+  
+  def launch!
+    introduction
+    
+    conclusion
+  end
+  
+  def introduction
+    puts "\n\n<<< Welcom to the Food Finder >>>\n\n"
+    puts "This is an interactive guide to help you find the food you crave.\n\n"
+  end
+  
+  def conclusion
+    puts "\n<<< Goodbye and Bon Appetit! >>>\n\n\n"
+  end
+  
+end
