9bb7848a7f6ee5462cc22e65dea14e565bf067d4
Mario Wang <mario.wang@autodesk.com>
2016-03-30 23:18:27 +0800

    Implemented guide class

--- food_finder/lib/guide.rb
+++ food_finder/lib/guide.rb
@@ -4,7 +4,7 @@ class Guide
   
   def initialize(path=nil)
     Restaurant.filepath = path
-    if Restaurant.file_exists?
+    if Restaurant.file_usable?
       puts "Found restaurant file."
     elsif Restaurant.create_file
       puts "Created restaurant file."
@@ -16,8 +16,29 @@ class Guide
   
   def launch!
     introduction
-    
+    result = nil
+    until result == :quit
+      print "> "
+      user_response = gets.chomp
+      result = do_action(user_response)
+    end    
     conclusion
+  end
+  
+  def do_action(action)
+#    puts "action: #{action}"
+    case action
+    when 'list'
+      puts "Listing..."
+    when 'find'
+      puts "Finding..."
+    when 'add'
+      puts "Adding..."
+    when 'quit'
+      return :quit
+    else
+      puts "\nI don't understand that command.\n"
+    end
   end
   
   def introduction
--- food_finder/lib/restaurant.rb
+++ food_finder/lib/restaurant.rb
@@ -1,16 +1,30 @@
 class Restaurant
   
   @@filepath = nil
+  
   def self.filepath=(path=nil)
     @@filepath = File.join(APP_ROOT, path)
   end
   
   def self.file_exists?
-    
+    if @@filepath && File.exists?(@@filepath)
+      return true
+    else
+      return false
+    end
+  end
+  
+  def self.file_usable?
+    return false unless @@filepath
+    return false unless File.exists?(@@filepath)
+    return false unless File.readable?(@@filepath)
+    return false unless File.writable?(@@filepath)
+    return true
   end
   
   def self.create_file
-    
+    File.open(@@filepath, 'w') unless file_exists?
+    return file_usable?
   end
   
   def self.saved_restaurants
